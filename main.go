d6e83e306d0b01bbde1b45f0be8159bf13fb17ab
unknown <marinoskouvaras@gmail.com>
2024-11-10 12:29:43 +0200

    part2

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
-package main 
+package main
 
 import (
-	"os"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
 func (p *Page) save() error {
@@ -19,14 +21,21 @@ func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
-		return nil,err
-	}	
+		return nil, err
+	}
 	return &Page{Title: title, Body: body}, nil
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a simple page.")}
-	p1.save()
-	p2,_ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a simple page.")}
+	// p1.save()
+	// p2,_ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
 }
